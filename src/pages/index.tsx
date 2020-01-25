79323ea6894df281748042cdada65742e2ae806f
jtinianow <jermtee@gmail.com>
2020-01-25 10:44:20 -0700

    add the token list to the homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,14 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import TokenList from '../components/TokenList/TokenList'
+import tokens from '../../build/js/variables-scss'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
+    <TokenList tokens={tokens} />
   </Layout>
 )
 
