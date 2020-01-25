044ceb2497d79ece92692d6e5b64a18c914ebb25
jtinianow <jermtee@gmail.com>
2020-01-25 10:41:18 -0700

    add token list

---
+++ src/components/TokenList/TokenList.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import TokenListItem from '../TokenListItem/TokenListItem'
+
+interface TokenListProps {
+    tokens: any
+}
+
+const TokenList = (props: TokenListProps) => {
+    const { tokens } = props
+
+    return (
+        <section className="token-list">
+            {Object.keys(tokens).map(key => {
+                const type = tokens[key];
+
+                return (
+                    <div key={key} className="token-list__section">
+                        <h2 className="token-list__section-header">{key}</h2>
+                        {type.name && type.value &&
+                            <TokenListItem name={type.name} value={type.value} attributes={type.attributes} />
+                        }
+
+                        {!type.value && Object.keys(type).map(typeKey => {
+                            const item = type[typeKey];
+
+                            return (
+                                <div key={typeKey}>
+                                    {/* <h3>{typeKey}</h3> */}
+                                    {item.name && item.value &&
+                                        <TokenListItem name={item.name} value={item.value} attributes={item.attributes} />
+                                    }
+
+                                    {!item.value && Object.keys(item).map(itemKey => {
+                                        const subItem = item[itemKey];
+
+                                        return (
+                                            <div key={itemKey}>
+                                                {/* <h4>{itemKey}</h4> */}
+                                                {subItem.name && subItem.value &&
+                                                    <TokenListItem name={subItem.name} value={subItem.value} attributes={subItem.attributes} />
+                                                }
+
+                                                {!subItem.value && Object.keys(subItem).map(stateKey => {
+                                                    const state = subItem[stateKey];
+
+                                                    return (
+                                                        <div key={stateKey}>
+                                                            {/* <p>{stateKey}</p> */}
+                                                            {state.name && state.value &&
+                                                                <TokenListItem name={state.name} value={state.value} attributes={state.attributes} />
+                                                            }
+                                                        </div>
+                                                    )
+                                                })}
+                                            </div>
+                                        )
+                                    })}
+                                </div>
+                            )
+                        })}
+                    </div>
+                )
+            })}
+        </section>
+    )
+}
+
+export default TokenList
