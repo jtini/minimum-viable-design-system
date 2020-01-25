7804d9d1f817e359b52250772538d86f22b316bc
jtinianow <jermtee@gmail.com>
2020-01-25 10:42:04 -0700

    build tokens as js

---
+++ build/js/variables-scss.js
@@ -0,0 +1,116 @@
+/**
+ * Do not edit directly
+ * Generated on Sat, 25 Jan 2020 17:41:46 GMT
+ */
+
+module.exports = {
+  "color": {
+    "text": {
+      "value": "#111111",
+      "original": {
+        "value": "#111"
+      },
+      "name": "color-text",
+      "attributes": {
+        "category": "color",
+        "type": "text"
+      },
+      "path": [
+        "color",
+        "text"
+      ]
+    },
+    "background": {
+      "value": "#ffffff",
+      "original": {
+        "value": "#fff"
+      },
+      "name": "color-background",
+      "attributes": {
+        "category": "color",
+        "type": "background"
+      },
+      "path": [
+        "color",
+        "background"
+      ]
+    }
+  },
+  "type": {
+    "family": {
+      "value": "system",
+      "original": {
+        "value": "system"
+      },
+      "name": "type-family",
+      "attributes": {
+        "category": "type",
+        "type": "family"
+      },
+      "path": [
+        "type",
+        "family"
+      ]
+    },
+    "size": {
+      "value": 16,
+      "original": {
+        "value": 16
+      },
+      "name": "type-size",
+      "attributes": {
+        "category": "type",
+        "type": "size"
+      },
+      "path": [
+        "type",
+        "size"
+      ]
+    },
+    "weight": {
+      "value": "normal",
+      "original": {
+        "value": "normal"
+      },
+      "name": "type-weight",
+      "attributes": {
+        "category": "type",
+        "type": "weight"
+      },
+      "path": [
+        "type",
+        "weight"
+      ]
+    },
+    "style": {
+      "value": "normal",
+      "original": {
+        "value": "normal"
+      },
+      "name": "type-style",
+      "attributes": {
+        "category": "type",
+        "type": "style"
+      },
+      "path": [
+        "type",
+        "style"
+      ]
+    },
+    "line-height": {
+      "value": 1.25,
+      "original": {
+        "value": 1.25
+      },
+      "name": "type-line-height",
+      "attributes": {
+        "category": "type",
+        "type": "line-height"
+      },
+      "path": [
+        "type",
+        "line-height"
+      ]
+    }
+  }
+};
